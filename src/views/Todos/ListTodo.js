06246357c176fef8b5f5bd639d402faabf5871a7
hungmanh02 <domanh462@gmail.com>
2024-04-03 11:17:48 +0700

    edit todos, update todo toast success

--- src/views/Todos/ListTodo.js
+++ src/views/Todos/ListTodo.js
@@ -18,6 +18,7 @@ class ListTodo extends Component {
         title: "Fixing bugs",
       },
     ],
+    editTodo: {},
   };
   addNewTodo = (todo) => {
     let currentListTodo = this.state.listTodos;
@@ -28,8 +29,47 @@ class ListTodo extends Component {
     });
     toast.success("Wow so easy!");
   };
+  handleDeleteTodo = (todo) => {
+    let currentListTodo = this.state.listTodos;
+    currentListTodo = currentListTodo.filter((item) => item.id !== todo.id); //tạo ra mảng mới và loại cái todo.id chon delete
+    this.setState({
+      listTodos: currentListTodo,
+    });
+    toast.success("Delete success!");
+  };
+  handleEditTodo = (todo) => {
+    let { editTodo, listTodos } = this.state;
+    let isEmptyObj = Object.keys(editTodo).length === 0;
+    //Save
+    if (isEmptyObj === false && editTodo.id === todo.id) {
+      let listTodosCopy = [...listTodos];
+      let objIndex = listTodosCopy.findIndex((item) => item.id === todo.id);
+      // save new data in posion array list Todo
+      listTodosCopy[objIndex].title = editTodo.title;
+      // save setState
+      this.setState({
+        listTodos: listTodosCopy,
+        editTodo: {},
+      });
+      toast.success("Update Todo success !");
+      return;
+    }
+
+    // Edit
+    this.setState({
+      editTodo: todo,
+    });
+  };
+  handleOnChangeEditTodo = (event) => {
+    let editTodoCopy = { ...this.state.editTodo };
+    editTodoCopy.title = event.target.value;
+    this.setState({
+      editTodo: editTodoCopy,
+    });
+  };
   render() {
-    let { listTodos } = this.state;
+    let { listTodos, editTodo } = this.state;
+    let isEmptyObj = Object.keys(editTodo).length === 0;
     return (
       <>
         <div className="list-todo-container">
@@ -40,11 +80,44 @@ class ListTodo extends Component {
               listTodos.map((item, index) => {
                 return (
                   <div className="todo-child" key={item.id}>
-                    <span>
-                      {index + 1} - {item.title}
-                    </span>
-                    <button className="edit">Edit</button>
-                    <button>Delete</button>
+                    {isEmptyObj === true ? (
+                      <span>
+                        {index + 1} - {item.title}
+                      </span>
+                    ) : (
+                      <>
+                        {editTodo.id === item.id ? (
+                          <span>
+                            {index + 1}
+                            <input
+                              onChange={(event) =>
+                                this.handleOnChangeEditTodo(event)
+                              }
+                              value={editTodo.title}
+                            />
+                          </span>
+                        ) : (
+                          <span>
+                            {index + 1} - {item.title}
+                          </span>
+                        )}
+                      </>
+                    )}
+
+                    <button
+                      className="edit"
+                      onClick={() => this.handleEditTodo(item)}
+                    >
+                      {isEmptyObj === false && editTodo.id === item.id
+                        ? "Save"
+                        : "Edit"}
+                    </button>
+                    <button
+                      className="delete"
+                      onClick={() => this.handleDeleteTodo(item)}
+                    >
+                      Delete
+                    </button>
                   </div>
                 );
               })}
@@ -60,9 +133,7 @@ class ListTodo extends Component {
           pauseOnFocusLoss
           draggable
           pauseOnHover
-          theme="dark"
         />
-        <ToastContainer />
       </>
     );
   }
